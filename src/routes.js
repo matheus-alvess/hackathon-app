cee4e03bf00f0efcd4e297d5bed94789c8059023
matheus-alvess <matheus.maatt@gmail.com>
2020-02-15 15:20:53 -0300

    adicionando orm sequelize ao projeto

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.get('/ping', (req, res) => {
-  res.json({ message: 'ping' });
+  res.json({ message: 'pong' });
 });
+
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 export default routes;
